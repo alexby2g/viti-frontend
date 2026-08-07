b3ed0919e9e3c68c7b749f7e4d3a8c459473665b
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-07 19:08:54 +0000

    Activar soporte offline en VITI web

--- quasar.config.js
+++ quasar.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from '#q-app/wrappers'
 export default defineConfig(() => ({
-  boot: ['theme', 'axios'],
+  boot: ['theme', 'axios', 'offline'],
   css: ['app.scss'],
   extras: ['material-icons'],
   build: { vueRouterMode: 'history', env: { API_URL: process.env.VITE_API_URL || 'http://localhost:8000/api/v1' } },
