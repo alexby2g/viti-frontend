3d7d72f86b387cb0133f64744491158f50d76638
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-07 19:09:22 +0000

    Activar sincronización push Android

--- quasar.config.js
+++ quasar.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from '#q-app/wrappers'
 export default defineConfig(() => ({
-  boot: ['theme', 'axios', 'offline'],
+  boot: ['theme', 'axios', 'offline', 'mobilePush'],
   css: ['app.scss'],
   extras: ['material-icons'],
   build: { vueRouterMode: 'history', env: { API_URL: process.env.VITE_API_URL || 'http://localhost:8000/api/v1' } },
