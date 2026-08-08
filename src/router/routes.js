85050512b1fb1b33b109ed5fcc4f51f9698c3907
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-08 14:22:24 +0000

    Registrar ruta del formulario público

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,7 @@ export default [
   { path: '/configuracion-inicial', component: AuthLayout, children: [{ path: '', name: 'setup', component: () => import('../pages/SetupPage.vue') }] },
   { path: '/registro', component: AuthLayout, children: [{ path: '', name: 'client-register', component: () => import('../pages/ClientRegisterPage.vue') }] },
   { path: '/registro-cliente/:token', component: AuthLayout, children: [{ path: '', name: 'client-onboarding', component: () => import('../pages/ClientOnboardingPage.vue') }] },
+  { path: '/solicitud', component: AuthLayout, children: [{ path: '', name: 'public-request-start', component: () => import('../pages/PublicSolicitudStartPage.vue') }] },
   { path: '/solicitar/:token', component: AuthLayout, children: [{ path: '', name: 'public-request', component: () => import('../pages/PublicSolicitudPage.vue') }] },
   { path: '/login', component: AuthLayout, children: [{ path: '', name: 'login', component: () => import('../pages/LoginPage.vue') }] },
   {
