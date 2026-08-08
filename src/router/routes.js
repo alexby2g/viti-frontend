340a5f7e16eb0ca92e7865b00ca2a16ff7cb41eb
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-08 00:36:31 +0000

    Agregar ruta de almacenamiento

--- src/router/routes.js
+++ src/router/routes.js
@@ -24,6 +24,7 @@ export default [
       { path: 'reportes', name: 'reportes', component: () => import('../pages/ReportesPage.vue'), meta:{adminOnly:true} },
       { path: 'buzon', name: 'buzon', component: () => import('../pages/BuzonPage.vue'), meta:{adminOnly:true} },
       { path: 'auditoria', name: 'auditoria', component: () => import('../pages/AuditoriaPage.vue'), meta:{adminOnly:true} },
+      { path: 'almacenamiento', name: 'almacenamiento', component: () => import('../pages/StoragePage.vue'), meta:{adminOnly:true} },
     ],
   },
   { path: '/:catchAll(.*)*', redirect: '/' },
