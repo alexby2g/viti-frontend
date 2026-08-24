07763af2432caec9a4633dcdaf83dc90c56a8fbe
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-24 00:12:34 -0400

    feat(006): mount floating assistant from root app

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,8 @@
-<template><router-view /></template>
+<script setup>
+import Agr006FloatingAssistant from './components/agr/Agr006FloatingAssistant.vue'
+</script>
+
+<template>
+  <router-view />
+  <Agr006FloatingAssistant />
+</template>
